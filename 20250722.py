9110e4d2913d18a4845dcf849729bbec0e51577b
hwan <devhwan0@gmail.com>
2025-07-22 22:54:46 +0900

    20250722

---
+++ 20250722.py
@@ -0,0 +1,20 @@
+# python book
+https://wikidocs.net/book/1
+
+# python -> C 랑 결합 가능
+# Mojo -> python wrapping language
+
+# 기계어 -> 0, 1
+# 컴파일러 -> 번역기
+# 컴파일 언어 (속도 빠름) ex. C -> 작성 후 컴파일러를 먼저 돌린다 -> 기계어에 가까운 실행 프로그램 만들기 -> 실행
+# 인터프리터 언어 (속도 느림) ex. python -> 한 줄 읽고 인터프리터 -> 실행 (반복)
+
+# python download
+https://www.python.org/
+
+# 상단 메뉴 Downloads -> Windows -> Ctrl + F (검색) -> Download Windows installer (64-bit)
+
+# Add python.exe to PATH 반드시 체크 -> Customize installation -> Optional Features 전부 체크 후 Next
+# -> Advanced Options 전부 체크 후 Browse 지정 (경로 지정) 후 Install -> 예 -> Setup was successful (완료)
+
+# lecture 2 26:03 - 20250722
