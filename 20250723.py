8ca49d50cd202311a7374cd8d89cc724d5248455
hwan <devhwan0@gmail.com>
2025-07-23 12:20:29 +0900

    20250723

---
+++ 20250723.py
@@ -0,0 +1,59 @@
+# >>>
+'''
+REPL
+파이썬 구문을 입력하면 바로 결과를 보여주고 다시 입력할 수 있는 도구
+Read Evaluate Print Loop
+종료 -> quit()
+'''
+
+# IDLE
+'''
+파일 만들기
+File -> New File -> 코드 입력 -> Ctrl + S (저장) -> Run -> Run Module
+'''
+
+# PowerShell
+'''
+Window + R (실행) -> 열기 (powershell) -> 확인 -> 경로 이동 -> python fileName.py
+'''
+
+# Editor
+'''
+추천 에디터
+IDLE, 파이참, Visual Studio Code
+'''
+
+# Visual Studio Code
+'''
+https://code.visualstudio.com/
+
+Download for Windows
+
+동의합니다. 다음 -> 전부 체크. 다음 -> 설치 -> 종료
+
+Extensions -> python install
+
+PowerShell 에서 실행
+경로 이동 -> code .
+
+Ctrl + / (주석)
+'''
+
+# Colaboratory
+'''
+구글에서 만든 온라인 파이썬 실행 환경
+
+https://colab.research.google.com/
+
+코드 입력 -> Shift + Enter (실행)
+'''
+
+# IDE
+'''
+Integrated Development Environment
+통합개발환경
+'''
+
+print("Hwan")
+
+# lecture 2 finish - 20250723
